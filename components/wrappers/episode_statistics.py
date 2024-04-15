91e7624defd4318482bd94ecf8acad2b27caa374
user50lab <56128974+user50lab@users.noreply.github.com>
2024-04-15 21:00:26 +0800

    Update episode_statistics.py

--- components/wrappers/episode_statistics.py
+++ components/wrappers/episode_statistics.py
@@ -16,12 +16,13 @@ class EpisodeStatistics(Wrapper):
         self.ep_len = 0
         self.ep_ret = 0.0
 
+    ### 在一个环境或模拟中执行一步动作，并且更新了一些关于当前episode的统计信息。
     def step(self, actions):
         rewards, terminated, info = super(EpisodeStatistics, self).step(actions)
 
         self.ep_len += 1  # One step elapse.
         # Since reward filter may be used, use actual rewards as long as they are available.
-        self.ep_ret += info.get('actual_rewards', rewards).mean()  # Average cross agents.
+        self.ep_ret += info.get('actual_rewards', rewards).mean()  # Average cross agents.   # 更新当前episode的累计奖励。
 
         # When episode terminates, add episode statistics to info.
         if terminated:
